a862589b85cf0bfd1e0a02498104578581713724
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-03-28 11:31:11 -0600

    Add background to landing page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 setInterval(movePlane, 20);
 var keys = {};
 
+var plane1 = $('#plane1').position().left;
+    plane2 = $('#plane2').position().left;
+
+var pos1 = $('#plane1').position();
+    pos2 = $('#plane2').position();   
+
 $(document).keydown(function(e) {
     keys[e.keyCode] = true;
 });
@@ -15,20 +21,32 @@ function movePlane() {
         if (!keys.hasOwnProperty(direction)) continue;
         
         if (direction == 39) {
-            $("#plane1").animate({left: "+=5"}, 0);  
+            $("#plane1").animate({left: "+=5"}, 20);
+             pos1 = $('#plane1').position().left;
+          
+        
         }
         
         if (direction == 68) {
-            $("#plane2").animate({left: "+=5"}, 0);  
+            $("#plane2").animate({left: "+=5"}, 20); 
+             pos2 = $('#plane2').position().left;
         }
+      checkForWinner();
     }
 }
 
-var raceTime1 = Math.floor( (Math.random() * 5000) + 1);
-var raceTime2 = Math.floor( (Math.random() * 5000) + 1);
+function checkForWinner() {
+    console.log(pos1);
+    console.log("HEY");
+    if (pos1 >= 300) {
+        alert("You Win!");
+    }
+   
+}
 
 
-var isComplete = false;
+
+
 
 
 
