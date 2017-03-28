e49d0db2a34eb37e07ddcbc37047bbf478f40798
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-03-28 15:46:38 -0600

    Finished requirments

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-setInterval(movePlane, 20);
+var myVar = setInterval(movePlane, 70);
 var keys = {};
 
-var plane1 = $('#plane1').position().left;
-    plane2 = $('#plane2').position().left;
 
-var pos1 = $('#plane1').position();
-    pos2 = $('#plane2').position();   
+var pos1 = $('#plane1').position().left;
+    pos2 = $('#plane2').position().left;   
 
 $(document).keydown(function(e) {
     keys[e.keyCode] = true;
@@ -21,33 +19,42 @@ function movePlane() {
         if (!keys.hasOwnProperty(direction)) continue;
         
         if (direction == 39) {
-            $("#plane1").animate({left: "+=5"}, 20);
+            $("#plane1").animate({left: "+=5"}, 70);
              pos1 = $('#plane1').position().left;
-          
-        
-        }
+           }
         
         if (direction == 68) {
-            $("#plane2").animate({left: "+=5"}, 20); 
+            $("#plane2").animate({left: "+=5"}, 70); 
              pos2 = $('#plane2').position().left;
+                 console.log(pos2);
         }
       checkForWinner();
     }
+
 }
 
 function checkForWinner() {
-    console.log(pos1);
-    console.log("HEY");
-    if (pos1 >= 300) {
-        alert("You Win!");
-    }
+   console.log("checkforwinner");
+   console.log(pos1);
+   if (pos1 >= 500) {
+    //alert("Plane 1 wins!");
+    reset(1);
+   } else if (pos2 >= 500) {
+    //alert("Plane 2 wins!");
+    reset(2);
+
+   }
    
 }
 
 
-
-
-
+function reset(winner) {
+ alert("Player " + winner + " You won!");
+  clearInterval(myVar);
+  $('#plane1').position().left = 0;
+  $('#plane2').position().left = 0;  
+  
+}
 
 
 
