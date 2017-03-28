47d7d642a090db94bbcd40dcadd2311a06551ecb
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-03-27 20:38:39 -0600

    Deleted up and down moves for plane1

--- app.js
+++ app.js
@@ -16,16 +16,12 @@ function movePlane() {
         if (direction == 37) {
             $("#plane").animate({left: "-=5"}, 0);                
         }
-        if (direction == 38) {
-            $("#plane").animate({top: "-=5"}, 0);  
-        }
         if (direction == 39) {
             $("#plane").animate({left: "+=5"}, 0);  
         }
-        if (direction == 40) {
-            $("#plane").animate({top: "+=5"}, 0);  
+       
         }
-    }
+    
 }
 
 var newGame = function() {
